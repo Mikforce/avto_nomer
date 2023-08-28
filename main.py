c0b7436ff9d6f647edb3e31f919cdb182eb40836
Ivan <100131490+Mikforce@users.noreply.github.com>
2023-08-28 17:40:56 +0300

    1
    1

---
+++ main.py
@@ -0,0 +1,68 @@
+# pip install matplotlib pytesseract opencv-python
+from flask import Flask, render_template, request, redirect, url_for
+import subprocess
+import matplotlib.pyplot as plt
+import pytesseract
+import cv2
+
+app = Flask(__name__)
+
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+def process_image(img_path):
+    def open_img(img_path):
+        carplate_img = cv2.imread(img_path)
+        carplate_img = cv2.cvtColor(carplate_img, cv2.COLOR_BGR2RGB)
+        return carplate_img
+
+    def carplate_extract(image, carplate_haar_cascade):
+        carplate_rects = carplate_haar_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
+
+        for x, y, w, h in carplate_rects:
+            carplate_img = image[y+15:y+h-10, x+15:x+w-20]
+
+        return carplate_img
+
+    def enlarge_img(image, scale_percent):
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+        return resized_image
+
+    carplate_img_rgb = open_img(img_path)
+    carplate_haar_cascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
+
+    carplate_extract_img = carplate_extract(carplate_img_rgb, carplate_haar_cascade)
+    carplate_extract_img = enlarge_img(carplate_extract_img, 150)
+
+    carplate_extract_img_gray = cv2.cvtColor(carplate_extract_img, cv2.COLOR_RGB2GRAY)
+    carplate_text = pytesseract.image_to_string(
+        carplate_extract_img_gray,
+        config='--psm 6 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    )
+
+    return carplate_extract_img_gray, carplate_text
+
+@app.route('/save_photo', methods=['POST'])
+def save_photo():
+    image_data = request.json.get('image')
+
+    # Запускаем скрипт для обработки
+    processed_img, carplate_text = process_image(img_path='8402943.jpg')
+
+    result = f'Номер авто: {carplate_text}'
+    print(result)
+
+    return redirect(url_for('index', result=result))
+
+
+
+
+if __name__ == '__main__':
+    app.run()
