d568440e403c1ea9923e00226c2e723249713614
Ivan <100131490+Mikforce@users.noreply.github.com>
2023-08-29 18:11:43 +0300

    1
    1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# pip install matplotlib pytesseract opencv-python tesseract-ocr flask
 from flask import Flask, render_template, request, redirect, url_for, session
 import subprocess
 import matplotlib.pyplot as plt
